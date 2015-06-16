f65069154be8c326ad95e143c63aa5d06461ebef
Michael Vetter <g.bluehut@gmail.com>
2015-06-16 11:33:36 +0200

    Add osc module
    Add aliases and useful completion for SUSE osc tool.

---
+++ modules/osc.zsh
@@ -0,0 +1,6 @@
+# for SUSEs osc tool
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/:/}
+alias isc="osc -A ibs"
+alias oscb="osc build --ccache --cpio-bulk-download --download-api-only"
+alias oscsd="osc service localrun download_files"
