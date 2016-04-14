09fe6aeb10d61d23fd21d660ff4b9b36a2dcb041
Michael Vetter <jubalh@openmailbox.org>
2016-04-14 11:52:05 +0200

    add base16 module

---
+++ modules/base16-shell.zsh
@@ -0,0 +1,7 @@
+# random base16 theme
+#cd $HOME/src/base16-shell/
+#zsh $(ls *.sh | sort -R | tail -1)
+#cd $HOME
+
+#londontube, paraiso, flat, codeschool, harmonic16
+zsh /$HOME/src/base16-shell/base16-harmonic16.dark.sh
