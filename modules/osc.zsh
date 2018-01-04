dda9429ce10981eae1695a02a1ab52a5745c95d9
Michael Vetter <jubalh@iodoru.org>
2018-01-04 12:27:46 +0100

    add osc diff alias

--- modules/osc.zsh
+++ modules/osc.zsh
@@ -4,3 +4,4 @@ export COMP_WORDBREAKS=${COMP_WORDBREAKS/:/}
 alias isc="osc -A ibs"
 alias oscb="osc build --ccache --cpio-bulk-download --download-api-only"
 alias oscsd="osc service localrun download_files"
+alias oscd="osc diff | view -"
